33c4b88a5cf3a7187632f8156f273e36dfc6600a
Mike Devresse <mike.devresse@gmail.com>
2021-01-26 12:40:30 +0100

    Added more advanced tests for entity User

--- tests/unit/Entity/UserEntityTest.php
+++ tests/unit/Entity/UserEntityTest.php
@@ -1,23 +1,52 @@
 <?php
 
-
 namespace App\Tests\unit\Entity;
 
-
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class UserEntityTest extends TestCase
+class UserEntityTest extends KernelTestCase
 {
     public function testCreateUserWithUsername() {
         $user = new User();
         $user->setUsername('Newuser');
 
-        $this->assertEquals('newuser',$user->getUsername());
+        $this->assertNull($user->getId());
+        $this->assertEquals('Newuser',$user->getUsername());
         $this->assertIsArray($user->getRoles());
         $this->assertContains('ROLE_USER',$user->getRoles());
-
-        $this->assertNull($user->getPassword());
-        $this->assertNull($user->getSalt());
-
     }
+
+    public function testRoleChanging() {
+        $user = new User();
+
+        $this->assertIsArray($user->getRoles());
+        $this->assertEquals(['ROLE_USER'],$user->getRoles());
+
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $this->assertIsArray($user->getRoles());
+        $this->assertContains('ROLE_ADMIN',$user->getRoles());
+        $this->assertContains('ROLE_USER',$user->getRoles());
+    }
+
+    public function testEncodingPassword() {
+        $user = new User();
+        self::bootKernel();
+        $passwordEncoder = self::$container->get('security.password_encoder');
+
+        $user->setPassword($passwordEncoder->encodePassword(
+            $user,
+            'testpassword'
+        ));
+
+        $this->assertNotEquals('testpassword',$user->getPassword());
+
+        $this->assertTrue($passwordEncoder->isPasswordValid($user,'testpassword'));
+
+        $this->assertNull($user->getSalt());
+        $this->assertNull($user->eraseCredentials());
+    }
+
 }
